eb6e299f6dbfeefad5031f740c8f4c6a4c9e003d
Nathan Mora <104747322+nathanmorax@users.noreply.github.com>
2026-05-11 11:09:19 -0600

    .

--- Podcast/Representation/Episodes/EpisodesController.swift
+++ Podcast/Representation/Episodes/EpisodesController.swift
@@ -243,39 +243,3 @@ extension EpisodesController: UICollectionViewDelegateFlowLayout {
 
 // MARK: - HostingHeaderView
 
-final class HostingHeaderView: UICollectionReusableView {
-    
-    private var hostingController: UIHostingController<AnyView>?
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .systemBackground
-    }
-    
-    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
-    func host<Content: View>(_ view: Content) {
-        hostingController?.view.removeFromSuperview()
-        hostingController = nil
-        
-        let hosting = UIHostingController(rootView: AnyView(view))
-        hosting.view.translatesAutoresizingMaskIntoConstraints = false
-        hosting.view.backgroundColor = .clear
-        addSubview(hosting.view)
-        
-        NSLayoutConstraint.activate([
-            hosting.view.topAnchor.constraint(equalTo: topAnchor),
-            hosting.view.leadingAnchor.constraint(equalTo: leadingAnchor),
-            hosting.view.trailingAnchor.constraint(equalTo: trailingAnchor),
-            hosting.view.bottomAnchor.constraint(equalTo: bottomAnchor)
-        ])
-        
-        self.hostingController = hosting
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        hostingController?.view.removeFromSuperview()
-        hostingController = nil
-    }
-}
